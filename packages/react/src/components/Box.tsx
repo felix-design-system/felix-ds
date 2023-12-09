565ba8f49a9025865074495a92f4f882d796674f
Jefferson Felix <jefferson@felixtech.dev>
2023-12-09 14:45:26 -0300

    fix(Box): className merging

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type BoxProps = {
   className?: string
   children: ReactNode
 }
 
-export function Box({ children }: BoxProps) {
+export function Box({ children, className }: BoxProps) {
   return (
-    <div className="p-4 rounded-md bg-neutral-50 dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-700">
+    <div
+      className={twMerge(
+        'p-4 rounded-md bg-neutral-50 dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-700',
+        className,
+      )}
+    >
       {children}
     </div>
   )
